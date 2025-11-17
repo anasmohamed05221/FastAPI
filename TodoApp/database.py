2a2351e1522c99f34efd929cb018be76732bdc7b
Anas Mohamed <anasabdelrhman397@gmail.com>
2025-11-17 18:34:37 +0200

    updated production database

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,8 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-#cloud postgres setup
-SQLALCHEMY_DATABASE_URL = "postgresql://avnadmin:AVNS_cT41ONPcQKztgdv0WnG@pg-e96814a-deploy-postgres-db.h.aivencloud.com:11233/defaultdb?sslmode=require"
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
+)
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 #sqlite setup
