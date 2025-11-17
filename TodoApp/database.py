b4307f39fd622d4d2fa5a487f0093cbbed031456
Anas Mohamed <anasabdelrhman397@gmail.com>
2025-11-17 18:41:25 +0200

    upgraded postgres production db

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,22 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
 
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
 
-SQLALCHEMY_DATABASE_URL = (
-    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
-)
+SQLALCHEMY_DATABASE_URL = "postgresql://avnadmin:AVNS_cT41ONPcQKztgdv0WnG@pg-e96814a-deploy-postgres-db.h.aivencloud.com:11233/defaultdb?sslmode=require"
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-#sqlite setup
-#SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
-#engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
